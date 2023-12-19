11c4ac936189a5f77af15fb0c3629ffc5de39bb2
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-12-19 10:59:39 -0600

    finished lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = []
+
+    for num in num_list:
+        if num % 2 == 0:
+            new_list.append(num)
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+   new_list = []
+
+   for list in sentence_list:
+         new_list.append(list + "!")
+   return new_list
+    
